e5200249267bcbd33c64c86560a6ebcbd62f0af9
Taskin Malek <taskinmalek8482@gmail.com>
2025-01-27 00:41:51 +0530

    Login API call on FE side

--- frontend/src/app/views/sessions/login/Login.jsx
+++ frontend/src/app/views/sessions/login/Login.jsx
@@ -75,9 +75,14 @@ const FirebaseRoot = styled("div")(({ theme }) => ({
 }));
 
 const LOGIN_MUTATION = gql`
-  mutation Login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
+      user {
+        userId
+        name
+        email
+      }
     }
   }
 `;
